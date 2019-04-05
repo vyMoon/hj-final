a851295a7b2c915578120c8a2d7c1675edfce079
vyMoon <46728118+vyMoon@users.noreply.github.com>
2019-04-06 01:49:01 +0300

    Update variables.js

--- js/variables.js
+++ js/variables.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const apiURL = 'https://neto-api.herokuapp.com';
 
 const app = finder('.app');
@@ -21,20 +19,18 @@ let shiftX = 0;
 let shiftY = 0;
 
 // картинка и ошибки при работе скартинуой
-const img = finder('.current-image');
+const img = finder('.current-image');           // слой с картинкой
 let imgSrc = false;                             // нужна для того чтобы показывать ошибку при дропе на имеющееся изображение
 const imgTypeError = finder('.error');          // контейнер с ошибкой при загрузке не файла не верного типа
 const imgLoader = finder('.image-loader');      // отображается при загрузке картинки
 
 // хоолст
-// const doodle = document.createElement('canvas');
-// doodle.classList.add('canvas');
 const doodle = elementMaker('canvas', 'canvas');
 app.insertBefore(doodle, img.nextElementSibling);
 const ctx = doodle.getContext("2d");
 
+// маска
 const mask = elementMaker('img', 'mask');
-// mask.src = 'example.png';
 app.insertBefore(mask, doodle);
 
 // переменные для рисования
@@ -58,12 +54,13 @@ const paintingColors = {    // доступные цвета
     purple: '#b36ade'
 };
 
-// фктивный цвет рисования
+// активный цвет рисования
 // ищем инпут, который  чекнут по умолчанию и присваиваем переменной код выбранного цвета
 let actualColor = paintingColors[ colorInputs.find( el => {return el.hasAttribute('checked')} ).value ];
 
+//инпуты, скрывающие - показывающие комментарии
 const commentsToggleInputs = finderAll('.menu__toggle', menuCommentsTools);
-// console.log(commentsToggleInputs[0].value);
+
 
 const commentsForm = finder('.comments__form');
 const commentsContainer = document.createElement('div');
@@ -71,16 +68,6 @@ commentsContainer.classList.add('commentsContainer');
 commentsContainer.style.position = 'relative';
 app.insertBefore(commentsContainer, doodle.nextElementSibling);
 
-// const commentExample = commentsForm.cloneNode(true);
-
-// commentExample.children[2].children[1].parentElement.removeChild(commentExample.children[2].children[1])
-// commentExample.children[2].children[1].parentElement.removeChild(commentExample.children[2].children[1])
-
-// commentExample.children[2].children[0].style.display = 'none';
-// commentExample.children[2].children[1].style.display = 'none';
-
-// const commentText = finder('.comment', finder('.comments__body', commentsForm));
-
-// let date = new Date();
-// let ws;
-
+console.log(window.location)
+console.log(window.location.search);
+console.log(window.location.pathname + window.location.search)
